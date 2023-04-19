be9ae14af96cb6e77492c3fb60e5a315f3619618
mykolaromanets <125486364+mykolaromanets@users.noreply.github.com>
2023-04-19 22:28:25 +0300

    fixed Task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,7 +3,7 @@ console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach((category) => {
   const categoryTitle = category.firstElementChild.textContent;
-  const categoryElement = category.children[1];
+  const categoryElement = category.lastElementChild;
   const numberOfItems = categoryElement.children.length;
   console.log(`Category: ${categoryTitle}
     Elements: ${numberOfItems}`);
